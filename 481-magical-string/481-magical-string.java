bc145b7022a603de0d4fc5d29848c28195c1e446
Jaskaran Singh <89383365+Jaskaran-Techno@users.noreply.github.com>
2022-05-05 18:27:54 +0530

    Time: 5 ms (79.84%), Space: 42.9 MB (55.81%) - LeetHub

---
+++ 481-magical-string/481-magical-string.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int magicalString(int n) {
+      if (n <= 0) {
+            return 0;
+        }
+        if (n <= 3) {
+            return 1;
+        }
+
+        int[] arr = new int[n + 1];
+        arr[0] = 1; arr[1] = 2; arr[2] = 2;
+        int head = 2, tail = 3, num = 1, result = 1;
+
+        while (tail < n) {
+            for (int i = 0; i < arr[head]; i++) {
+                arr[tail] = num;
+                if (num == 1 && tail < n) {
+                    result++;
+                }
+                tail++;
+            }
+            num = num ^ 3;
+            head++;
+        }
+
+        return result;  
+    }
+}
